7d5bf036888224cc76db9d528f05cfb51b80e392
Patrick Mullot <pmullot@gmail.com>
2021-01-13 22:48:17 +0100

    update cred

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyDlHWtqmpHY8wHRI3Ds58oNb6l_yg63n8E',
-    authDomain: 'code-mentoring-edition1.firebaseapp.com',
-    projectId: 'code-mentoring-edition1',
-    storageBucket: 'code-mentoring-edition1.appspot.com',
-    messagingSenderId: '521748620768',
-    appId: '1:521748620768:web:f5c29a463ce62255a87785',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
     measurementId: 'G-M0W7VV3HLF',
   },
   collections: {
