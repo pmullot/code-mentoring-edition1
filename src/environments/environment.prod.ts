0e0600daeb0f844daae4ef762141bef2dd07a66c
Patrick Mullot <pmullot@gmail.com>
2021-01-13 22:53:07 +0100

    update cred

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyDlHWtqmpHY8wHRI3Ds58oNb6l_yg63n8E',
-    authDomain: 'code-mentoring-edition1.firebaseapp.com',
-    projectId: 'code-mentoring-edition1',
-    storageBucket: 'code-mentoring-edition1.appspot.com',
-    messagingSenderId: '521748620768',
-    appId: '1:521748620768:web:f5c29a463ce62255a87785',
-    measurementId: 'G-M0W7VV3HLF',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
   },
   collections: {
     venues: 'venues',
