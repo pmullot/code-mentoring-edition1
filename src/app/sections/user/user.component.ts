f6495b9f571edb5fde5f14745794b0e0eaea3a0e
Thomas Z Lukoma <tzlukoma@gmail.com>
2021-01-26 15:53:32 -0500

    Review of changes to the user component

--- src/app/sections/user/user.component.ts
+++ src/app/sections/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // Adds functionality for working with forms
 import { AuthService } from '@shared/services/auth.service';
 import { UsersService } from '@shared/services/users.service';
 import { filter, take } from 'rxjs/operators';
@@ -13,17 +13,21 @@ export class UserComponent {
   public userForm: FormGroup;
 
   constructor(protected _fb: FormBuilder, protected _authService: AuthService, protected _userService: UsersService) {
+    // Step 1: Build the form
     this.buildForm();
+    // Step 2: Retrieve and subscribe to the user information for authenticated user
     this._authService.user$
       .pipe(
         filter((user) => !!user),
         take(1)
       )
       .subscribe((user) => {
+        // Step 3: Fill the form with any of the user values that are there in the autheticated user
         this.userForm.patchValue(user);
       });
   }
 
+  // Builds the form using the Formgroup.group() method
   protected buildForm(): void {
     this.userForm = this._fb.group({
       name: [null, Validators.required],
@@ -36,6 +40,7 @@ export class UserComponent {
     });
   }
 
+  // Used on the onClick handler in the form to submit the values in the form to the database
   public async onSubmit(): Promise<void> {
     if (this.userForm.dirty && this.userForm.valid) {
       await this._userService.saveUser(this.userForm.value);
