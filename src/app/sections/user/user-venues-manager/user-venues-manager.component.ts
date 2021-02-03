548f6207bcf1bd0cd021367f57c9853f85bf77ad
Thomas Z Lukoma <tzlukoma@gmail.com>
2021-02-03 09:39:54 -0500

    Adds form for adding new venue

--- src/app/sections/user/user-venues-manager/user-venues-manager.component.ts
+++ src/app/sections/user/user-venues-manager/user-venues-manager.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // Adds functionality for working with forms
 import { VenuesService } from '@sections/venues/services/venues.service';
 import { Venue } from '@core/models/venue.model';
 
@@ -8,10 +9,29 @@ import { Venue } from '@core/models/venue.model';
   styleUrls: ['./user-venues-manager.component.scss'],
 })
 export class UserVenuesManagerComponent {
+  public venueForm: FormGroup;
   public userVenues: Venue[]
 
-  constructor(protected _venueService: VenuesService) {
+  constructor(protected _venueService: VenuesService, protected _fb: FormBuilder) {
     this.userVenues = this._venueService.getVenues()
 
+    this.buildVenueForm();
+    // Step 2: Retrieve and subscribe to the user information for authenticated user
+
   }
+
+  // Builds the form using the Formgroup.group() method
+  protected buildVenueForm(): void {
+    this.venueForm = this._fb.group({
+      name: [null, Validators.required],
+      zipCode: null,
+    });
+  }
+
+  public async onSubmit(): Promise<void> {
+    if (this.venueForm.dirty && this.venueForm.valid) {
+      await this._venueService.saveVenue({ ...this.venueForm.getRawValue(), owner: 'tzlukoma@gmail.com' });
+    }
+  }
+
 }
--- src/app/sections/venues/services/venues.service.ts
+++ src/app/sections/venues/services/venues.service.ts
@@ -1,9 +1,11 @@
+import { AngularFireAuth } from '@angular/fire/auth';
 import { User } from './../../../core/models/user.model';
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AuthService } from '@shared/services/auth.service'
 import { Venue } from '@core/models/venue.model';
 import { BehaviorSubject } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
 import { environment } from '@env';
 import { Observable } from 'rxjs';
 
@@ -16,9 +18,9 @@ export class VenuesService {
   public venues$: BehaviorSubject<Venue[]> = new BehaviorSubject(null);
   // Declare local variable to hold the user
   public currentUser: User
-  constructor(protected _afs: AngularFirestore, protected _authService: AuthService) {
+  constructor(protected _afAuth: AngularFireAuth, protected _afs: AngularFirestore, protected _authService: AuthService) {
     // Use the auth service to get the user from the behavior subject and save that into the local variable
-    this.currentUser = this._authService.getUser()
+    //TODO
     // When I console log the value below, it is null
     console.log(`currentUser`, this.currentUser)
 
@@ -43,10 +45,16 @@ export class VenuesService {
     } else if (!venue.name) {
       throw new Error('each venue must have a name')
     }
+
+    console.log(venue)
     return this._afs
       .collection(VENUE_COL)
       .doc().set(venue, { merge: true })
       .then((_) => venue)
+  }
+
+  async setUser() {
+    this.currentUser = await this._authService.getUser()
   }
 
 
